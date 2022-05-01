159b2a2e7da2def1aa9ade602fc2d5e7065fd66a
pec <pec@DESKTOP-JBDBHD6>
2022-05-01 21:14:22 +0530

    new code for log

--- MyStoreProject/src/main/java/com/mystore/utility/Log.java
+++ MyStoreProject/src/main/java/com/mystore/utility/Log.java
@@ -15,36 +15,5 @@ public class Log {
 		Log.info("====================================="+sTestCaseName+" TEST END=========================================");
 		 }
 	
-	// Need to create below methods, so that they can be called  
-
-	 public static void info(String message) {
-
-			Log.info(message);
-
-			}
-
-	 public static void warn(String message) {
-
-	    Log.warn(message);
-
-		}
-
-	 public static void error(String message) {
-
-	    Log.error(message);
-
-		}
-
-	 public static void fatal(String message) {
-
-	    Log.fatal(message);
-
-		}
-
-	 public static void debug(String message) {
-
-	    Log.debug(message);
-
-		}
 	
 }
